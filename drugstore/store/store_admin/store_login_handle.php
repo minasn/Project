975c0b848a7db4d2a93211bd705587615a1c55be
HapSlience <761046327@qq.com>
2016-04-07 15:41:02 +0800

    完成了store的登陆 session数据为store session销毁没做

---
+++ drugstore/store/store_admin/store_login_handle.php
@@ -0,0 +1,46 @@
+<?php 
+	require_once('../connect.php');
+	session_start();
+	$_SESSION['store']=false;
+
+	$store_tele=$_POST['store_tele'];
+	$store_password=$_POST['store_password'];
+	if(!preg_match("/^[0-9]{11}/",$store_tele)){
+		echo "<script> alert('手机号格式错误')</script>";
+		// echo "<script> document.location='../store_login.html'</script>";
+		changePage("store_login.html");
+	}
+
+	$query="select * from t_store where store_username=$store_tele and store_password=$store_password";
+	$result=mysql_query($query);
+	$isFind=mysql_num_rows($result);
+	echo $isFind;
+
+	if($isFind==1){
+		$_SESSION['store']=true;
+		changePage("store_index.html");
+
+
+	}else{
+		echo "<script> alert('用户密码错误')</script>";
+		// echo "<script> document.location='../store_login.html'</script>";
+		changePage();
+	}
+	
+
+
+
+
+
+
+
+
+	function changePage($address){
+		echo "<script> document.location='../$address'</script>";
+	}
+
+
+
+
+ ?>
+ 
